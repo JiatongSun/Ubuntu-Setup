e54c0aec6d84c9a159173364fa6550fc508b3cdc
JiatongSun <SJT_sjtu1998@163.com>
2020-12-02 20:43:27 -0500

    update

--- ubuntu_quick_start.sh
+++ ubuntu_quick_start.sh
@@ -58,5 +58,18 @@ sudo deepin-wine-for-ubuntu/install.sh -y
 wget -qO- https://deepin-wine.i-m.dev/setup.sh | sudo sh
 sudo apt-get install deepin.com.wechat -y
 
+# Install Typora
+wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
+sudo add-apt-repository 'deb https://typora.io/linux ./'
+sudo apt-get update
+sudo apt-get install typora -y
+
+# Install Sublime
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get install sublime-text -y
+
+
 
 
