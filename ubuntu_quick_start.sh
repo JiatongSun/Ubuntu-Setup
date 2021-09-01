f9928171237bff2f2714a1688f71abc6f4863a17
JiatongSun <SJT_sjtu1998@163.com>
2021-08-31 20:45:44 -0400

    fix shell script bug

--- ubuntu_quick_start.sh
+++ ubuntu_quick_start.sh
@@ -11,7 +11,7 @@ sudo apt install curl -y
 
 # Install Zsh
 sudo apt install zsh -y
-chsh -s /usr/bin/zsh
+# chsh -s /usr/bin/zsh  # Change bash to zsh
 
 # Install Oh-My-Zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -34,7 +34,7 @@ gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 
 # Install Gnome Tweaks
 sudo apt install gnome-tweaks -y
-sudo apt install chrome-gnome-shell
+sudo apt install chrome-gnome-shell -y
 
 # Install Git
 sudo apt install git -y
@@ -59,7 +59,7 @@ pip3 install numpy scipy pandas matplotlib
 # Install Sogou Pinyin (Please download sogou from https://pinyin.sogou.com/linux/)
 sudo apt install fcitx-bin -y
 sudo apt-get install fcitx-table -y
-# sudo dpkg -i sogoupinyin*.deb 
+# sudo dpkg -i sogoupinyin*.deb # Install Sogou Pinyin
 
 # Install WeChat
 git clone "https://gitee.com/wszqkzqk/deepin-wine-for-ubuntu.git"
@@ -75,7 +75,7 @@ sudo apt-get install typora -y
 
 # Install Sublime
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt-get install apt-transport-https
+sudo apt-get install apt-transport-https -y
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install sublime-text -y
@@ -101,8 +101,8 @@ curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo ap
 sudo apt update
 sudo apt install ros-noetic-desktop-full -y
 source /opt/ros/noetic/setup.zsh
-echo "source /opt/ros/noetic/setup.zsh" >> ~/.zshrc
-source ~/.zshrc
+echo "source /opt/ros/noetic/setup.zsh" >> ~/.bashrc
+source ~/.bashrc
 sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential -y
 sudo rosdep init
 rosdep update
